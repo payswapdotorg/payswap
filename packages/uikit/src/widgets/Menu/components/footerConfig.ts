8c2a89c15367765ba3c65c12654c34ff522df47e
pectoraux <ekontetevi@gmail.com>
2024-03-28 14:31:06 +0000

    175

--- packages/uikit/src/widgets/Menu/components/footerConfig.ts
+++ packages/uikit/src/widgets/Menu/components/footerConfig.ts
@@ -58,7 +58,7 @@ export const footerLinks: (t: ContextApi["t"]) => FooterLinkType[] = (t) => [
     items: [
       {
         label: "Github",
-        href: "https://github.com/pectoraux",
+        href: "https://github.com/payswapdotorg",
       },
       // {
       //   label: t("Documentation"),
